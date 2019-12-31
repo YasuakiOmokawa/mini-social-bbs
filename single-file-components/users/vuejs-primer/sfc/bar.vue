1823bf4aaec36dd9c310cd22f67d61b57e8627bd
面川泰明 <omokawa@senk-inc.co.jp>
2019-12-31 11:21:10 +0900

    add sfc entry

---
+++ single-file-components/users/vuejs-primer/sfc/bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです</p>
+    <!-- Fooコンポーネントに対してheaderクラスを指定すると意図しない動きになる！ -->
+    <!-- <foo class="header" /> -->
+  </div>
+</template>
+
+<script>
+import Foo from "./foo";
+export default {
+  components: {
+    Foo
+  }
+};
+</script>
+
+<style scoped>
+.bar {
+  border: solid 1px red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 125%;
+}
+</style>
---
+++ single-file-components/users/vuejs-primer/sfc/foo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="foo">
+    <h1 class="header">Fooコンポーネント</h1>
+    <p>これはFooコンポーネントです</p>
+  </div>
+</template>
