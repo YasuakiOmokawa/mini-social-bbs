dd9ebb24a6bbc68456da5bbe904cdc4cadba369e
面川泰明 <omokawa@senk-inc.co.jp>
2019-12-31 15:52:42 +0900

    create sfc example

---
+++ single-file-components/users/vuejs-primer/sfc/css-module/root.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="root">
+    <h1 class="header">Rootコンポーネント</h1>
+    <p>これはRootコンポーネントです。</p>
+    <foo /><!-- Fooコンポーネントの利用。scriptブロックも要注目 -->
+    <bar /><!-- Barコンポーネントの利用。scriptブロックも要注目 -->
+  </div>
+</template>
+<script>
+import Foo from "./foo";
+import Bar from "./bar";
+export default {
+  components: { Foo, Bar }
+};
+</script>
+<style>
+#root {
+  border: solid 1px blue;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 200%;
+}
+p {
+  text-decoration: underline;
+}
+</style>
---
+++ single-file-components/users/vuejs-primer/sfc/multilingal-support/lang.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  p.message メッセージ: {{msg}}
+</template>
+
+<script lang="ts">
+export default {
+  data(): Object {
+    return { msg: "こんにちは！" };
+  }
+};
+</script>
+
+<style lang="stylus">
+vue-color = #42b983;
+
+.message {
+  color: vue-color;
+}
+</style>
